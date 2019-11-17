f142ffcc3ca07bdc37332a882d3da63fec4a98f1
Bryan Triana <bryantrianarueda@gmail.com>
2019-11-16 20:10:35 -0500

    First commit

---
+++ app/src/main/java/com/inversepalindrome/jibberjabber/MainActivity.java
@@ -0,0 +1,22 @@
+/*
+Copyright (c) 2019 Inverse Palindrome
+Jibber Jabber - MainActivity.java
+https://inversepalindrome.com/
+*/
+
+
+package com.inversepalindrome.jibberjabber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
