cd1ae8b70736d3ac8aa32a4079a6c11182f8bb51
Bryan Triana <bryantrianarueda@gmail.com>
2019-11-23 17:34:53 -0500

    Added profile fragment edit buttons

--- app/src/main/java/com/inversepalindrome/jibberjabber/ProfileFragment.java
+++ app/src/main/java/com/inversepalindrome/jibberjabber/ProfileFragment.java
@@ -7,20 +7,90 @@ https://inversepalindrome.com/
 
 package com.inversepalindrome.jibberjabber;
 
-import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.ImageButton;
 
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements OnClickListener{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        nameText = view.findViewById(R.id.profile_profile_entry);
+        aboutText = view.findViewById(R.id.profile_about_entry);
+        editNameButton = view.findViewById(R.id.edit_name_button);
+        editAboutButton = view.findViewById(R.id.edit_about_button);
+
+        editNameButton.setOnClickListener(this);
+        editAboutButton.setOnClickListener(this);
+
+        return view;
     }
+
+    @Override
+    public void onClick(View view){
+        switch(view.getId())
+        {
+            case R.id.edit_name_button:
+                onEditName(view);
+                break;
+            case R.id.edit_about_button:
+                onEditAbout(view);
+                break;
+        }
+    }
+
+    private void onEditName(View view){
+        AlertDialog nameDialog = new AlertDialog.Builder(getActivity()).create();
+        nameDialog.setTitle("Edit Name");
+
+        nameDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        nameDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        nameDialog.show();
+    }
+
+    private void onEditAbout(View view){
+        AlertDialog aboutDialog = new AlertDialog. Builder(getActivity()).create();
+        aboutDialog.setTitle("Edit About");
+        aboutDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        aboutDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        aboutDialog.show();
+    }
+
+    private TextView nameText;
+    private TextView aboutText;
+    private ImageButton editNameButton;
+    private ImageButton editAboutButton;
 }
